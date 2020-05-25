b7e01b6da47021c19396950e0fdbf3af76f621a8
Yash0018 <59160508+Yash0018@users.noreply.github.com>
2020-05-25 16:28:31 +1000

    This branch is to add addition feature to calculator project

--- Calculator/Addition.cs
+++ Calculator/Addition.cs
@@ -10,10 +10,6 @@ namespace Calculator
     {
         public override decimal Calculate(decimal first, decimal second)
         {
-            if (second == 0)
-            {
-                return first;
-            }
             return first + second;
         }
     }
--- Calculator/TestAddition.cs
+++ Calculator/TestAddition.cs
@@ -18,18 +18,18 @@ namespace Calculator
         }
         [TestCase(1, 2, 3)]
         [TestCase(0.1,0.2,0.3)]
-        public void Addding_Positive_Numbers(decimal first, decimal second, decimal result)
+        public void Testaddingbothpositive(decimal first, decimal second, decimal result)
         {
             Assert.AreEqual(result, addition.Calculate(first, second));
         }
         [TestCase(-2,3.5,1.5)]
         [TestCase(-3.5,2,-1.5)]
-        public void Addding_negative_positive_Numbers(decimal first, decimal second, decimal result)
+        public void Testaddingpositivenegative(decimal first, decimal second, decimal result)
         {
             Assert.AreEqual(result, addition.Calculate(first, second));
         }
         [TestCase(-3,-4,-7)]
-        public void Adding_both_negative_Numbers(int first, int second, int result)
+        public void Testaddingbothnegative(int first, int second, int result)
         {
             Assert.AreEqual(result, addition.Calculate(first, second));
         }
