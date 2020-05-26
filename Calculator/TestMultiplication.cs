60b14c419466abe80564257a87d11e46ec86da91
Priyam-Swine <priyamj580@gmail.com>
2020-05-26 16:24:57 +1000

    multiplication and division class with tests

--- Calculator/TestMultiplication.cs
+++ Calculator/TestMultiplication.cs
@@ -11,18 +11,21 @@ namespace Calculator
         {
             multiplication = new Multiplication();
         }
+
         [TestCase(2, 1, 2)]
         [TestCase(0.5, 0.7, 0.35)]
         public void TestMultiplyingBothPositive(decimal first, decimal second, decimal result)
         {
             Assert.AreEqual(result, multiplication.Calculate(first, second));
         }
+
         [TestCase(-4, 3.1, -12.4)]
         [TestCase(6, -2.2, -13.2)]
         public void TestMultiplyingPositiveNegative(decimal first, decimal second, decimal result)
         {
             Assert.AreEqual(result, multiplication.Calculate(first, second));
         }
+
         [TestCase(-5, -7, 35)]
         public void TestMultiplyingBothNegative(int first, int second, int result)
         {
