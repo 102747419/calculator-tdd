13fffc513bec411c07fbc14518f55670290de9b8
Priyam-Swine <priyamj580@gmail.com>
2020-05-26 16:07:42 +1000

    multiplication and division class with tests

---
+++ Calculator/Division.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Calculator
+{
+    public class Division : Operator
+    {
+        public override decimal Calculate(decimal first, decimal second)
+        {
+            return first / second;
+        }
+    }
+}
